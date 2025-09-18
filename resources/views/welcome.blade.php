f2ded7bb5019455979c3789a7f104e69f4a05658
Rob Barclay <156030496+rob4236@users.noreply.github.com>
2025-09-18 11:36:56 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+aison<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Menu Maison...
                 </div>
 
                 <div class="links">
