e8dc9d16af0816f70a7aecd336a3809d6ef2a893
Rob Barclay <rob@allianceipos.com>
2025-09-19 23:39:16 +0100

    set cookie back to laravel

--- config/session.php
+++ config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'Menu Maison'), '_').'_session'
+        Str::slug(env('APP_NAME', 'Laravel'), '_').'_session'
     ),
 
     /*
